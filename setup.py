4c74a55c12bdd4d0f4a3f077b4eb97637377f538
Marvin Alvarenga <marvinalexz@gmail.com>
2020-11-27 22:24:19 -0600

    📝 Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple-banking-system",
+    version="1.0.0",
+    author="Marvin Alvarenga",
+    author_email="marvinalexz@gmail.com",
+    description="A simple use of design patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marvinAlvarenga/SimpleBankingSystem",
+    packages=setuptools.find_packages(),
+    scripts=['run.py'],
+)
